59f6acab5553eb526089c80809b050819c7e4f05
YangXJ95 <47676704+YangXJ95@users.noreply.github.com>
2019-03-31 21:41:11 -0700

    Update func_shape_fit.m

--- CV/cv_part/func_shape_fit.m
+++ CV/cv_part/func_shape_fit.m
@@ -1,36 +1,77 @@
 close all, clear all, clc
 
 
-ball_centers = [1,0,0;
-               0 1 0;
-               0 0 1];
-           
-p1 = ball_centers(1,:);
-p2 = ball_centers(2,:);
-p3 = ball_centers(3,:);
+% holder center to ball center = 33 mm
+ball_centers = [0, -33, 0;
+                33, 0, 0;
+                0, 33, 0];           
+% add noise
+ball_centers = ball_centers+rand(3)
 
-x = ( (p2(2)-p1(2))*(p3(3)-p1(3))-(p2(3)-p1(3))*(p3(2)-p1(2)) );
-y = ( (p2(3)-p1(3))*(p3(1)-p1(1))-(p2(1)-p1(1))*(p3(3)-p1(3)) );
-z = ( (p2(1)-p1(1))*(p3(2)-p1(2))-(p2(2)-p1(2))*(p3(1)-p1(1)) );
+% v_x: center to yellow direction 
+% v_y: green to red direction
+[RAVEN_center,v_x,v_y] = end_effector_PR(ball_centers)
 
-norm = [x,y,z]/sqrt(x^2+y^2+z^2)
-
-syms cx cy cz d1x d1y d1_z d2_x d2_y d2_z 
-
-eqn_1 = 
-
-function eqn = end_effector_PR(x) 
-    T=ravenFK(x(1),x(2),x(3),x(4),x(5),x(6));
-    T_takeout = T(1:3,:);
+function  [RAVEN_center,v_x,v_y] = end_effector_PR(ball_centers)
     
-    eqn = [];
-    for iter = 1:12
-        eqn = [eqn;T_takeout(iter)-T_fact_takeout(iter)];
-    end
-end
+    p0 = ball_centers(1,:); 
+    p1 = ball_centers(2,:);
+    p2 = ball_centers(3,:);
+    center = (p0+p2)/2;
 
-options = optimoptions('fsolve','FunctionTolerance',1e-100,'StepTolerance',1e-100,'MaxIter',10000,'Display','off');
-input = fsolve(@IK,initial_guess,options);
+    nx = ( (p1(2)-p0(2))*(p2(3)-p0(3))-(p1(3)-p0(3))*(p2(2)-p0(2)) );
+    ny = ( (p1(3)-p0(3))*(p2(1)-p0(1))-(p1(1)-p0(1))*(p2(3)-p0(3)) );
+    nz = ( (p1(1)-p0(1))*(p2(2)-p0(2))-(p1(2)-p0(2))*(p2(1)-p0(1)) );
+
+    % norm to the three point plane
+    n = [nx,ny,nz]/sqrt(nx^2+ny^2+nz^2);
+
+    function eqn = end_effector_PR(theta) 
+        % rotation matrix (plane norm as rotation axis) 
+        a = cos(theta/2);
+        b = n(1)*sin(theta/2);
+        c = n(2)*sin(theta/2);
+        d = n(3)*sin(theta/2);
+        R = [a*a + b*b - c*c - d*d, 2*(b*c - a*d), 2*(b*d + a*c);
+            2*(b*c + a*d), a*a + c*c - b*b - d*d, 2*(c*d - a*b);
+            2*(b*d - a*c), 2*(c*d + a*b), a*a + d*d - b*b - c*c];
+        
+        % v_x: center to yellow as x axis
+        v_x = R*(p1'-center')/norm(p1-center);
+        
+        % v_y: green_to_red as y direction = cross(norm,d1)
+        v_y = cross(n,v_x);
+        
+        % holder center to ball center 
+        d = 33;
+        
+        % where ball(0,1,2) center is supposed to be
+        b0 = center-d*v_y; 
+        b1 = center+d*v_x; 
+        b2 = center+d*v_y; 
+        
+        % sum of difference between real ball center and supposed one
+        eqn = norm(b0-p0)+norm(b1-p1)+norm(b2-p2);
+    end
+
+    initial_guess = 0;
+    options = optimoptions('fsolve','FunctionTolerance',1e-100,'StepTolerance',1e-100,'MaxIter',10000,'Display','off');
+    theta = fsolve(@end_effector_PR,initial_guess,options);
+    
+    a = cos(theta/2);
+    b = n(1)*sin(theta/2);
+    c = n(2)*sin(theta/2);
+    d = n(3)*sin(theta/2);
+    R = [a*a + b*b - c*c - d*d, 2*(b*c - a*d), 2*(b*d + a*c);
+        2*(b*c + a*d), a*a + c*c - b*b - d*d, 2*(c*d - a*b);
+        2*(b*d - a*c), 2*(c*d + a*b), a*a + d*d - b*b - c*c];
+
+    v_x = (R*(p1'-center')/norm(p1-center))';
+    v_y = cross(n,v_x);
+    
+    % the RAVENcenter is at (-14.5mm, 0) 
+    RAVEN_center = center+v_x*(-14.5);
+end
 
         
 
