688baf0612ed8a7f1f1f658b08b66b752e27f07d
YangXJ95 <47676704+YangXJ95@users.noreply.github.com>
2019-03-31 22:17:19 -0700

    Update func_shape_fit.m

--- CV/cv_part/func_shape_fit.m
+++ CV/cv_part/func_shape_fit.m
@@ -1,10 +1,10 @@
 close all, clear all, clc
 
-
-% holder center to ball center = 33 mm
-ball_centers = [0, -33, 0;
-                33, 0, 0;
-                0, 33, 0];           
+%% sample
+% holder center to ball center = 33 mm, holder center at origin
+ball_centers = [0, -33, 0; %ball_0
+                33, 0, 0;  %ball_1
+                0, 33, 0]; %ball_2          
 % add noise
 ball_centers = ball_centers+rand(3)
 
@@ -12,12 +12,17 @@ ball_centers = ball_centers+rand(3)
 % v_y: green to red direction
 [RAVEN_center,v_x,v_y] = end_effector_PR(ball_centers)
 
+
+%% function
 function  [RAVEN_center,v_x,v_y] = end_effector_PR(ball_centers)
+    % holder center to ball center 
+    distance = 33;
     
     p0 = ball_centers(1,:); 
     p1 = ball_centers(2,:);
     p2 = ball_centers(3,:);
-    center = (p0+p2)/2;
+    mid = (p0+p1+p2)/3;
+    center = ((p0+p2)/2 + (mid-p1)*distance/norm(mid-p1)+p1)/2;
 
     nx = ( (p1(2)-p0(2))*(p2(3)-p0(3))-(p1(3)-p0(3))*(p2(2)-p0(2)) );
     ny = ( (p1(3)-p0(3))*(p2(1)-p0(1))-(p1(1)-p0(1))*(p2(3)-p0(3)) );
@@ -42,13 +47,10 @@ function  [RAVEN_center,v_x,v_y] = end_effector_PR(ball_centers)
         % v_y: green_to_red as y direction = cross(norm,d1)
         v_y = cross(n,v_x);
         
-        % holder center to ball center 
-        d = 33;
-        
         % where ball(0,1,2) center is supposed to be
-        b0 = center-d*v_y; 
-        b1 = center+d*v_x; 
-        b2 = center+d*v_y; 
+        b0 = center-distance*v_y; 
+        b1 = center+distance*v_x; 
+        b2 = center+distance*v_y; 
         
         % sum of difference between real ball center and supposed one
         eqn = norm(b0-p0)+norm(b1-p1)+norm(b2-p2);
